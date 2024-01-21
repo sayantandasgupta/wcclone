f41c547a80de67b0c43539683b5b04fe14028496
Sayantan <sayantan.dasgupta21@gmail.com>
2024-01-21 11:32:56 +0530

    Added count lines option functionality

--- wcclone.py
+++ wcclone.py
@@ -1,15 +1,27 @@
 import click
 
 @click.command()
+@click.option('-c', '--bytes', is_flag=True, default=False, help="print the byte counts")
+@click.option('-l', '--lines', is_flag=True, default=False, help="print the newline counts")
 @click.argument('file')
-def count(file):
+def main(bytes, lines, file):
+    
+    if lines:
+        try:
+            res = countLines(file)
+            click.echo(res)
+        except FileNotFoundError:
+            click.echo("File not found")
+
+def countLines(file : str) -> str:
     try:
-        f = open(file)
-    except FileNotFoundError as E:
-        click.echo("File not found")
-
-
-    # click.echo(f"Sum is {sum}")
+        with open(file) as f:
+            lines = f.readlines()
+            numLines = len(lines)
+            
+            return f"{numLines} {file}"
+    except FileNotFoundError:
+        raise FileNotFoundError
 
 if __name__ == "__main__":
-    count()
+    main()
